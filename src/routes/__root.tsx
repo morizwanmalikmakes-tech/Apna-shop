a9120dbbd44ef99dc40ef12a131bf4fb0c36d4c0
morizwanmalikmakes-tech <morizwanmalikmakes@gmail.com>
2026-08-09 13:55:42 +0530

    replace old favicon with kulhad logo

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -96,7 +96,7 @@ export const Route = createRootRouteWithContext<{ queryClient: QueryClient }>()(
     ],
     links: [
       { rel: "stylesheet", href: appCss },
-      { rel: "icon", href: "/favicon.webp", type: "image/webp" },
+      { rel: "icon", href: "/favicon.ico", type: "image/x-icon" },
       { rel: "preconnect", href: "https://fonts.googleapis.com" },
       { rel: "preconnect", href: "https://fonts.gstatic.com", crossOrigin: "anonymous" },
       {
